86459c7855a53407fd4d32024610a7b12d6eb4a0
rmeno12 <menonrahul02@gmail.com>
2020-06-04 17:05:22 -0500

    Add verbosity command line argument

--- front_to_back.py
+++ front_to_back.py
@@ -1,9 +1,16 @@
 from calculation import calculate_time
+import argparse
 
 def front_to_back(passengers):
     return sorted(passengers, key = lambda x: x[1])
 
-passengers = [(i, j) for i in range(1) for j in range(10)]
-passengers = front_to_back(passengers)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--verbose", help="increase verbosity of output", 
+                         action="store_true")
+    args = parser.parse_args()
 
-print(calculate_time(passengers, True))
+    passengers = [(i, j) for i in range(2) for j in range(50)]
+    passengers = front_to_back(passengers)
+
+    print(calculate_time(passengers, args.verbose))
