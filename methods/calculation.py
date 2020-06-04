1e9ba3d2ab98dd3fd1f15bbef94f1828e07386f0
rmeno12 <menonrahul02@gmail.com>
2020-06-04 14:59:21 -0500

    Add verbosity flag

--- methods/calculation.py
+++ methods/calculation.py
@@ -1,26 +1,26 @@
-def calculate_time(people):
-	print()
-	print("Seating", people)
+def calculate_time(people, verbose=False):
+	if verbose: print()
+	if verbose: print("Seating", people)
 	if len(people) <= 1:
-		print("Seating one or fewer people so no wait")
+		if verbose: print("Seating one or fewer people so no wait")
 		return 0
 	row = people[0][1]
 	total_time = 0
 	last_seated = people[0]
-	print("Currently with", people[0], "at row", row)
+	if verbose: print("Currently with", people[0], "at row", row)
 	for index, person in enumerate(people[1:]):
-		print(person, "can go to row", row - index - 1)
+		if verbose: print(person, "can go to row", row - index - 1)
 		if person[1] <= row - index - 1 and person[1] != row:
-			print("Person and row match!")
+			if verbose: print("Person and row match!")
 			last_seated = person
 			total_time += len(people[1:index+1])
-			print("Splitting into", people[1:index+1], "and", people[index+1:])
-			print("People have waited", total_time, "units in the first section")
-			total_time += calculate_time(people[1:index+1]) + calculate_time(people[index+1:])
+			if verbose: print("Splitting into", people[1:index+1], "and", people[index+1:])
+			if verbose: print("People have waited", total_time, "units in the first section")
+			total_time += calculate_time(people[1:index+1], verbose) + calculate_time(people[index+1:], verbose)
 			break
 	else:
-		print("No one lined up, moving everyone forward")
+		if verbose: print("No one lined up, moving everyone forward")
 		total_time += len(people[1:])
-		print("People have waited", total_time, "units this section")
-		total_time += calculate_time(people[1:])
+		if verbose: print("People have waited", total_time, "units this section")
+		total_time += calculate_time(people[1:], verbose)
 	return total_time
