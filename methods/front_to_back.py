81d966d9b52d8a4913a0b6315e99b09fc1c491b1
rmeno12 <menonrahul02@gmail.com>
2020-06-04 16:02:54 -0500

    Create front_to_back file and function

---
+++ methods/front_to_back.py
@@ -0,0 +1,9 @@
+from calculation import calculate_time
+
+def front_to_back(passengers):
+    return sorted(passengers, key = lambda x: x[1])
+
+passengers = [(i, j) for i in range(1) for j in range(10)]
+passengers = front_to_back(passengers)
+
+print(calculate_time(passengers, True))
