df83a46a9b0de1eee2868f507f30c03a68c46586
rmeno12 <menonrahul02@gmail.com>
2020-06-04 20:34:59 -0500

    Change plane rows and width to be a command line argument

--- front_to_back.py
+++ front_to_back.py
@@ -8,9 +8,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--verbose", help="increase verbosity of output", 
                          action="store_true")
+    parser.add_argument("rows", help="number of rows in the plane")
+    parser.add_argument("width", help="number of seats per row in the plane")
     args = parser.parse_args()
 
-    passengers = [(i, j) for i in range(2) for j in range(50)]
+    passengers = [(i, j) for i in range(int(args.width)) for j in range(int(args.rows))]
     passengers = front_to_back(passengers)
 
     print(calculate_time(passengers, args.verbose))
